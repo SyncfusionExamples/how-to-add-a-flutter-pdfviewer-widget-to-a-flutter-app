568872fdeae36a311e1531625a25bd6232da8f2a
immankumarsync <immankumar.p@syncfusion.com>
2024-10-16 13:53:47 +0530

    Update UI widgets

--- lib/main.dart
+++ lib/main.dart
@@ -10,36 +10,18 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Syncfusion Flutter PDF Viewer Demo',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(title: 'Syncfusion Flutter PDF Viewer Demo'),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
 
   final String title;
 
@@ -70,26 +52,17 @@ class _MyHomePageState extends State<MyHomePage> {
               onPressed: () {
                 _pdfViewerStateKey.currentState!.openBookmarkView();
               },
-              icon: Icon(
-                Icons.bookmark,
-                color: Colors.white,
-              )),
+              icon: Icon(Icons.bookmark)),
           IconButton(
               onPressed: () {
                 _pdfViewerController.jumpToPage(5);
               },
-              icon: Icon(
-                Icons.arrow_drop_down_circle,
-                color: Colors.white,
-              )),
+              icon: Icon(Icons.arrow_drop_down_circle)),
           IconButton(
               onPressed: () {
                 _pdfViewerController.zoomLevel = 1.25;
               },
-              icon: Icon(
-                Icons.zoom_in,
-                color: Colors.white,
-              ))
+              icon: Icon(Icons.zoom_in))
         ],
       ),
     ));
